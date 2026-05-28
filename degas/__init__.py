ba34310b15b6eda52129026f7aa7a338df1fb6f0
williamgilpin <williamgilpin@gmail.com>
2026-05-28 17:13:40 -0600

    latest

--- degas/__init__.py
+++ degas/__init__.py
@@ -2,7 +2,7 @@ from importlib.resources import files
 
 from .degas import *
 from .vernalis import cmap_vn
-from .vernalis import vernalis
+from .vernalis import cmap_vn as vernalis
 
 # Path to the styles directory (Python 3.9+)
 data_path = str(files(__package__) / "styles")
